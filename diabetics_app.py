60820d4b47b847986dccd8e6f4804c97966be521
CIRIL SHAJU <75011401+ciril9955@users.noreply.github.com>
2023-06-28 14:16:51 +0530

    Add files via upload

--- diabetics_app.py
+++ diabetics_app.py
@@ -1,29 +1,33 @@
 import streamlit as st
 import pickle
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
 
-# loading the saved model
-loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+# Loading the saved model
+loaded_model = pickle.load(open('trained_model.pkl', 'rb'))
 
 def diabetics_prediction(input_data):
-    # changing the input_data to numpy array
+    # Changing the input_data to a NumPy array
     input_data_as_numpy_array = np.asarray(input_data)
 
-    # reshape the array as we are predicting for one instance
+    # Reshape the array as we are predicting for one instance
     input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+
+    # Load the scaler used during training
+    sc = pickle.load(open('scaler.pkl', 'rb'))
 
     # Scale the input data using the same StandardScaler instance used for training
     input_data_scaled = sc.transform(input_data_reshaped)
 
     prediction = loaded_model.predict(input_data_scaled)
-    print(prediction)
 
     if prediction == 0:
         return 'The person is not diabetic'
     else:
         return 'The person is diabetic'
 
-# web page
+# Web page
 def main():
     st.set_page_config(
         page_title='Diabetes Prediction App',
@@ -39,12 +43,31 @@ def main():
     BMI = st.text_input("BMI Value:")
     DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function Value:")
     Age = st.text_input("Age of the Person:")
-    diagnosis = ''
 
     if st.button('Test Result'):
-        diagnosis = diabetics_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
-
-    st.success(diagnosis)
+        if (
+            Pregnancies != "" and
+            Glucose != "" and
+            BloodPressure != "" and
+            SkinThickness != "" and
+            Insulin != "" and
+            BMI != "" and
+            DiabetesPedigreeFunction != "" and
+            Age != ""
+        ):
+            diagnosis = diabetics_prediction([
+                float(Pregnancies),
+                float(Glucose),
+                float(BloodPressure),
+                float(SkinThickness),
+                float(Insulin),
+                float(BMI),
+                float(DiabetesPedigreeFunction),
+                float(Age)
+            ])
+            st.success(diagnosis)
+        else:
+            st.warning("Please enter values for all the input features.")
 
 if __name__ == '__main__':
     main()
